375a32052695dc8c07dd5cc35b43bb52e00600de
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-11 18:42:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(str)
   str1 = str.split(" ")
   new_str = ""
-  str1.each do |letter|
-    new_str += letter.reverse
+  str1.each do |word|
+    new_str += word.reverse
   end
   new_str
 end
